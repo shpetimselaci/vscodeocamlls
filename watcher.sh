13110f1332a89cb5b71d45cb8952ef6bcf15863e
Shpetim Selaci <shselaci3@gmail.com>
2024-04-18 22:30:26 +0200

    Update watcher.sh

--- watcher.sh
+++ watcher.sh
@@ -1,7 +1,7 @@
 ## /bin/sh
-opam exec -- dune build -w &
-DUNE_CONFIG__GLOBAL_LOCK=disabled opam exec -- dune exec vscodeocamlls -w # &
-# dune fmt -w
+DUNE_CONFIG__GLOBAL_LOCK=disabled opam exec -- dune build -w &
+DUNE_CONFIG__GLOBAL_LOCK=disabled opam exec -- dune exec vscodeocamlls -w 
+# DUNE_CONFIG__GLOBAL_LOCK=disabled opam exec -- dune fmt -w
 p1=$!
 
 wait $p1
